bd0bf9c39bc004c6fab3826885164db7aef90768
TechBits <abumurabet@gmail.com>
2026-02-16 13:21:58 +0100

    C program that return 1

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+/**
+ * main - prints a code to standard error
+ *
+ * Return: 1
+ */
+int main (void)
+{
+	const char *msg = "\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	
+	write(2, msg, 59);
+
+		return 1;
+}
