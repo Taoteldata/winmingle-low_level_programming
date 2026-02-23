ce4f3079ffb4a108ce1b4f322ea9e39ed203beee
TechBits <abumurabet@gmail.com>
2026-02-23 11:46:05 +0100

    Print the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/** Author: Taofeek Abdulrokeeb A.
+ * Program: WinMingle Community C Training
+ * Description: Writing program that prints the alphabet in lower case.
+ */
+int main (void)
+{
+    char x = 'a';
+	while (x <= 'z') {
+		putchar (x);
+		x++;
+	}
+	putchar('\n');
+
+    return (0);
+}/* To build a program with puts */
