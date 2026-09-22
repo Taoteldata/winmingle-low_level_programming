243b79300496fcedad9d83187e9b81ca15dd9534
taofakstudios <taofakstudios@gmail.com>
2026-09-22 13:19:53 +0100

    Printing a string to stdout

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/** Writing a function that prints a string, followed by a new line, to stdout. */
+
+void _puts(char *str)
+{
+    int i = 0;
+
+    while(str[i] != '\0')
+    {
+        _putchar(str[i]);
+        i++;
+    }
+
+    _putchar('\n');
+}
