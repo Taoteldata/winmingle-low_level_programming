7a177b8c8dbfd80709d6caccc2b1c4030c0453af
taofakstudios <taofakstudios@gmail.com>
2026-09-22 13:17:07 +0100

    Printing the lenght of a string

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/** Writing a function that return the lenght of a string */
+
+int _strlen(char *s)
+{
+    int i = 0;
+
+    while(s[i] != '\0')
+    {
+        i++;
+    }
+
+    return (i);
+}
